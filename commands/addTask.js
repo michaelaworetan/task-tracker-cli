b7b33545613e04e3fa526f6acb6ec1f85a663451
Aworetan Michael <michaelaworetan@gmail.com>
2024-11-22 14:38:43 +0100

    created the addTask and integrated it inindex.js

---
+++ commands/addTask.js
@@ -0,0 +1,29 @@
+// This module adds a new task to the task list.
+const { readFile, writeFile } = require("../utils/fileHandler"); // Import the file utility functions
+const { v4: uuidv4 } = require("uuid"); // Import the UUID library for generating unique IDs.
+
+const addTask = ([description]) => {
+  //chack if a description was provided, if not log a message
+  if (!description) {
+    console.log("Error: Description is requires");
+    return;
+  }
+
+  const tasks = readFile(); // Read the existing tasks from the JSON file
+
+  // Create a new task object with necessary properties
+  const newTask = {
+    id: uuidv4(), // Generate a unique Id for the task
+    description, // Assign the provided description for the task
+    status: "todo", // Default  for a new task is 'todo'.
+    createdAt: new Date().toDateString(), // Record the current date and time.
+    updatedAt: new Date().toDateString(), // Initially, updatedAt is the same as createdAt.
+  };
+
+  tasks.push(newTask); // Add new task to the tasks array
+  writeFile(tasks); //Save the updated array back to the JSON file
+
+  console.log(`Task added successfully (ID: ${newTask.id})`); //log a succedd with the new Tasks Id
+};
+
+module.exports = addTask; // Export the addTask function for use in the main CLI.
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Main CLI application logic
+
+const addTask = require("./commands/addTask");
+
+const command = process.argv[2]; // Extract the command (e.g., "add") from the CLI arguments
+const args = process.argv.slice(3);
+
+switch (command) {
+  case "add":
+    addTask(args); // Call the `addTask` function with the arguments.
+    break;
+
+  default:
+    // Log an error message if the user provides an invalid command.
+    console.log("Invalid command. Use one of the following: add");
+}
